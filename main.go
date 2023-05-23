94f6c1cac4887f9da36cc69cb2e57db519f6f640
Sebastian009w <ia3266595@gmail.com>
2023-05-22 21:41:25 -0500

    Fiber Server

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("Api Brawl Star")
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Api Brawl Stars")
+	})
+
+	app.Listen(":8080")
 }
