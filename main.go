b0b796df98d91a3e654c774d76a19d5ba93af1b1
Sebastian009w <ia3266595@gmail.com>
2023-05-23 22:23:36 -0500

    prepare production

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"flag"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/itsbeensolong/api-brawl-stars/pkg/services"
 )
 
+var (
+	PORT = flag.String("port", ":3000", "Port to Listen On")
+	PROD = flag.Bool("prod", false, "Enable prefork in production")
+)
+
 func main() {
-	app := fiber.New()
+	flag.Parse()
+	app := fiber.New(fiber.Config{
+		Prefork: *PROD,
+	})
+
+	v1 := app.Group("/api/v1")
 
 	app.Get("/", services.App)
 
-	app.Listen(":3000")
+	v1.Get("brawlers", func(c *fiber.Ctx) error {
+		return c.SendString("brawlers")
+	})
+
+	log.Fatal(app.Listen(*PORT))
 }
