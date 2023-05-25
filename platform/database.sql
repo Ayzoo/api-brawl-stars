ad21f5dd1bac28429dd794efb48f144242538ace
Sebastian009w <ia3266595@gmail.com>
2023-05-24 19:46:44 -0500

    primary table

--- platform/database.sql
+++ platform/database.sql
@@ -1,3 +1,17 @@
 CREATE DATABASE bralwstars;
 
-CREATE TABLE brawlers;
+CREATE TABLE brawlers(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    image VARCHAR(255),
+    type VARCHAR(255),
+    category VARCHAR(255)
+);
+
+CREATE TABLE brawlers_stellar(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    brawler_id INT,
+    primary_stellar VARCHAR(255),
+    second_stellar VARCHAR(255),
+    FOREIGN KEY (brawler_id) REFERENCES brawlers(id)
+);
