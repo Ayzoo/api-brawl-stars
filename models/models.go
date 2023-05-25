f5094d78de1bc6b6f973fee3cf7bcd5e2e4c6cdc
Sebastian009w <ia3266595@gmail.com>
2023-05-24 21:00:57 -0500

    models modifed

--- models/models.go
+++ models/models.go
@@ -1,1 +1,17 @@
 package models
+
+type Super struct {
+	Name        string
+	Description string
+	Damage      string
+	Scope       string
+}
+type Brawler struct {
+	Name     string
+	Image    string
+	Type     string
+	Category string
+	Stellar  [2]string
+	Gadget   [2]string
+	Super
+}
--- utils/types.go
+++ utils/types.go
@@ -5,19 +5,3 @@ type Username struct {
 	Password string
 	Database string
 }
-
-type Super struct {
-	Name        string
-	Description string
-	Damage      string
-	Scope       string
-}
-type Brawler struct {
-	Name     string
-	Image    string
-	Type     string
-	Category string
-	Stellar  [2]string
-	Gadget   [2]string
-	Super
-}
