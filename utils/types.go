54785bda8661e7654f1113f7d34e185ea6f8e3f9
Sebastian009w <ia3266595@gmail.com>
2023-05-26 22:01:29 -0500

    admin struct #25

--- utils/types.go
+++ utils/types.go
@@ -1,7 +1,19 @@
 package utils
 
+import (
+	"text/template"
+
+	core "github.com/gofiber/template"
+)
+
 type Username struct {
 	User     string
 	Password string
 	Database string
+}
+
+type Engine struct {
+	core.Engine
+
+	Templates *template.Template
 }
