a0c45da607b87ce75392d984d51c0b3cb5ce9bcd
Sebastian009w <ia3266595@gmail.com>
2023-05-27 10:43:15 -0500

    Search by Id brawler #23

---
+++ services/SearchById.go
@@ -0,0 +1,54 @@
+package services
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/itsbeensolong/api-brawl-stars/models"
+	"github.com/itsbeensolong/api-brawl-stars/pkg/database"
+)
+
+func SearchById(c *fiber.Ctx) error {
+	_id := c.Params("id")
+	conn, err := database.MySQL()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	query, err := conn.Prepare("SELECT * FROM brawlers WHERE id = ?")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	brawler := models.Brawler{}
+	row := query.QueryRow(_id)
+
+	var id int
+	var (
+		name     string
+		image    string
+		types    string
+		category string
+	)
+	var (
+		stellar int
+		gadget  int
+		super   int
+	)
+	var stellars [2]string
+
+	err = row.Scan(&id, &name, &image, &types, &category, &stellar, &gadget, &super)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	brawler.Name = name
+	brawler.Image = image
+	brawler.Type = types
+	brawler.Category = category
+	brawler.Stellar = stellars
+
+	return c.JSON(fiber.Map{
+		"brawler": brawler,
+	})
+}
