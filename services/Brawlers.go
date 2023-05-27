71d83f7c5798a6baeb6bb136308cb35fcd3c855f
Sebastian009w <ia3266595@gmail.com>
2023-05-26 21:14:15 -0500

    query SELECT #21

--- services/Brawlers.go
+++ services/Brawlers.go
@@ -1,9 +1,29 @@
 package services
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/itsbeensolong/api-brawl-stars/pkg/database"
+)
 
 func Brawlers(c *fiber.Ctx) error {
+
+	conn, err := database.MySQL()
+
+	if err != nil {
+		panic("Hubo un error en la coneccion")
+	}
+
+	query, err := conn.Query("SELECT * FROM brawlers")
+
+	if err != nil {
+		panic("Hubo un error en la consulta")
+	}
+
+	for query.Next() {
+
+	}
+
 	return c.JSON(fiber.Map{
-		"example": "YES",
+		"Example": "YES",
 	})
 }
